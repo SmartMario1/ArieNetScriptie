bb69f0cb65c1b5966160c4a14b3ad840b964adf7
SmartMario1 <62243426+SmartMario1@users.noreply.github.com>
2026-05-22 12:59:49 +0200

    trial

--- job_rlaf_3col_cooc.sh
+++ job_rlaf_3col_cooc.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --mem=60G
-#SBATCH -t 24:00:00
+#SBATCH -t 2:00:00
 #SBATCH --output=slurm_rlaf_3col_cooc_%j.out
 #SBATCH --error=slurm_rlaf_3col_cooc_%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
