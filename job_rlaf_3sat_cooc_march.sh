eeb1e6ef563de319ba95d1c6c74933edcbbf7df1
Sander Hoffman <SmartMario22@gmail.com>
2026-06-01 10:20:50 +0200

    Update job_rlaf_3sat_cooc_march.sh

--- job_rlaf_3sat_cooc_march.sh
+++ job_rlaf_3sat_cooc_march.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --mem=60G
-#SBATCH -t 12:00:00
+#SBATCH -t 8:00:00
 #SBATCH --output=slurm_rlaf_3sat_cooc_march_%j.out
 #SBATCH --error=slurm_rlaf_3sat_cooc_march_%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
