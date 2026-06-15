4713dd10d4d05b75dbf341c14caf2b06eb9a1d3b
SmartMario1 <62243426+SmartMario1@users.noreply.github.com>
2026-06-15 20:16:12 +0200

    glucose+models

---
+++ glucose_unmodified/simp/depend.mk
@@ -0,0 +1,40 @@
+/home/sander/Thesis2/nsnet/glucose/simp/Main.o /home/sander/Thesis2/nsnet/glucose/simp/Main.or /home/sander/Thesis2/nsnet/glucose/simp/Main.od /home/sander/Thesis2/nsnet/glucose/simp/Main.op: /home/sander/Thesis2/nsnet/glucose/simp/Main.cc \
+ /home/sander/Thesis2/nsnet/glucose/simp/../utils/System.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/IntTypes.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../utils/ParseUtils.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../utils/Options.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Vec.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/XAlloc.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/Dimacs.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/SolverTypes.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Alg.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Map.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Alloc.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../simp/SimpSolver.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Queue.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/Solver.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Heap.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/BoundedQueue.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/Constants.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Clone.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/SolverStats.h
+/home/sander/Thesis2/nsnet/glucose/simp/SimpSolver.o /home/sander/Thesis2/nsnet/glucose/simp/SimpSolver.or /home/sander/Thesis2/nsnet/glucose/simp/SimpSolver.od /home/sander/Thesis2/nsnet/glucose/simp/SimpSolver.op: /home/sander/Thesis2/nsnet/glucose/simp/SimpSolver.cc \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Sort.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Vec.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/IntTypes.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/XAlloc.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../simp/SimpSolver.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Queue.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/Solver.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Heap.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Alg.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../utils/Options.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../utils/ParseUtils.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/SolverTypes.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Map.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Alloc.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/BoundedQueue.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/Constants.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../mtl/Clone.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../core/SolverStats.h \
+ /home/sander/Thesis2/nsnet/glucose/simp/../utils/System.h
