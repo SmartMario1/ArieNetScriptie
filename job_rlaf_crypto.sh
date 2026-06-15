9ffc05bcdfdb78988c3543bfb8e3764f8835c5fd
SmartMario1 <62243426+SmartMario1@users.noreply.github.com>
2026-06-15 20:42:06 +0200

    Update job_rlaf_crypto.sh

--- job_rlaf_crypto.sh
+++ job_rlaf_crypto.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --mem=60G
-#SBATCH -t 24:00:00
+#SBATCH -t 8:00:00
 #SBATCH --output=slurm_rlaf_crypto_%j.out
 #SBATCH --error=slurm_rlaf_crypto_%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -58,14 +58,14 @@ python -u train_arienet_rlaf.py \
     training.clip_ratio=0.2 \
     training.kl_penalty=0.1 \
     training.use_amp=true \
-    training.accum_steps=10 \
+    training.accum_steps=20 \
     training.target_stat=decisions \
     solver.solver=glucose \
     solver.num_workers=16 \
     "dataset.train_path=$TRAIN_PATH" \
     "dataset.val_path=$VAL_PATH" \
     dataset.num_process_workers=16 \
-    loader.batch_size=2 \
+    loader.batch_size=1 \
     loader.num_workers=0 \
     optim.lr=1e-4 \
     optim.weight_decay=0.0 \
